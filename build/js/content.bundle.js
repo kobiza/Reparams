fcb63a64ab7e900727513d26d55eda7d6f1e6c76
kobiza <kobiz@wix.com>
2023-10-29 23:21:16 +0200

    add build folder to git

---
+++ build/js/content.bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+
+console.log('hello from content');
+
+/******/ })()
+;
+//# sourceMappingURL=content.bundle.js.map
